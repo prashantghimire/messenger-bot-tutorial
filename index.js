cbedf1e9b2b1456b7da4362d70d03b49bb77fed6
Prashant Ghimire <prashant.ghimire@selu.edu>
2017-03-06 11:26:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/webhook/', function (req, res) {
 				//sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender, "My Leviton Received: " + text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
